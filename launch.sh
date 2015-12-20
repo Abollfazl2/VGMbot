88c6253fe37b5213c0244d36b5182deabe8e6c8f
Abollfazl2 <fallahabolfazl2@hotmail.com>
2015-12-20 11:45:48 -0800

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -11,8 +11,8 @@ update() {
 
 # Will install luarocks on THIS_DIR/.luarocks
 install_luarocks() {
-  git clone https://github.com/Abollfazl2/VGMbot.git
-  cd VGMbot
+    git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
   git checkout tags/v2.2.1 # Current stable
 
   PREFIX="$THIS_DIR/.luarocks"
